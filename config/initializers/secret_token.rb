b3febe753948b18aad4f8267b1c84f1410fff482
mayur <mayurvit@gmail.com>
2013-08-27 12:59:28 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectOne::Application.config.secret_key_base = '57feb4a578b838a8ef7995f1cba1e5604880b8ea5fbd3e5d6734c95b6e24d48b9ae960983729b3c5aead7623f68496bce4d38474d2d818f4fdf4ef0cd0a8e190'
